6c16e3472e6c1bfc8fcb7c564ea926b1f42779ab
Polya <polya>
2024-07-27 18:41:03 +0300

    fix indent

--- less/simple_dag_example.py
+++ less/simple_dag_example.py
@@ -24,39 +24,39 @@ start_date = datetime(2024, 25, 7),
 catchup = False,
 tags = ['example'],
 ) as dag:
-t1 = BashOperator(
-    task_id='print_date',
-    bash_comand='date',
-)
-t2 = BashOperator(
-    task_id='sleep',
-    depends_on_past=False,
-    bash_comand='sleep 5',
-    retries=3,
-)
-t1.doc_md = dedent(
-    """\
-### Task Documentation
-You can bla bla
-"""
-)
-dag.doc_md = __doc__
-dag.doc_md = """
-    This is a documentation placed anywhere
+    t1 = BashOperator(
+        task_id='print_date',
+        bash_comand='date',
+    )
+    t2 = BashOperator(
+        task_id='sleep',
+        depends_on_past=False,
+        bash_comand='sleep 5',
+        retries=3,
+    )
+    t1.doc_md = dedent(
+        """\
+    ### Task Documentation
+    You can bla bla
     """
-templated_command = dedent(
-    """
-    {% for i in range(5) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-    {% endfor %}
-    """
-# здесь используется шаблонизация через Jinja
-t3 = BashOperator(
-    task_id='templated',
-    depends_on_past=False,
-    bash_comand=templated_command,
-)
-# последовательность задач
-t1 >> [t2, t3]  # t2 и t3 после t1( то же самое что t2<< t1, t3<<t1)
+    )
+    dag.doc_md = __doc__
+    dag.doc_md = """
+        This is a documentation placed anywhere
+        """
+    templated_command = dedent(
+        """
+        {% for i in range(5) %}
+            echo "{{ ds }}"
+            echo "{{ macros.ds_add(ds, 7)}}"
+        {% endfor %}
+        """
+    # здесь используется шаблонизация через Jinja
+    t3 = BashOperator(
+        task_id='templated',
+        depends_on_past=False,
+        bash_comand=templated_command,
+    )
+    # последовательность задач
+    t1 >> [t2, t3]  # t2 и t3 после t1( то же самое что t2<< t1, t3<<t1)
 
